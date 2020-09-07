fa91a36bf9ed935f42717f95118d63801d2a022d
LukeLash <63415678+LukeLash@users.noreply.github.com>
2020-09-07 16:58:19 -0500

    Add files via upload

---
+++ mobileOnCampus.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  
+/*
+  document.getElementById("navBarsIcon").addEventListener("click", showMenu);
+
+    function showMenu() {
+      document.getElementById('dropMenu').setAttribute("style","background-color:yellow");
+      document.getElementById('dropMenu').style.height = '50vh';
+
+    }
+
+*/
+
+
+
+}
