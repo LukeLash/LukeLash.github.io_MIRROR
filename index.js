0345d1b28e7a5b705981ff7a48b2d4999f97d9af
LukeLash <63415678+LukeLash@users.noreply.github.com>
2020-09-17 19:12:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,27 +6,33 @@ window.onload = () => {
       document.getElementById("mobileDiv").style.pointerEvents = "all";
       element.style.opacity = "1";
       element.style.filter  = 'alpha(opacity=90)'; // IE fallback
-      document.getElementById('decoBarVert').addEventListener('click', function() {location.href = 'index2.html'}, false);
     }
-   
-  
-  function noScroll() {
-  window.scrollTo(0, 0); 
-}
-
-// add listener to disable scroll
-window.addEventListener('scroll', noScroll);
-
-// Remove listener to re-enable scroll
-window.removeEventListener('scroll', noScroll);*/
 
 // On Campus Button
   document.getElementById('Button1').addEventListener('click', function() {location.href = 'index.html'}, false);
 
 // In Industry Button
-  document.getElementById('Button2').addEventListener('click', function() {location.href = 'OnCampus.html'}, false);
+  //document.getElementById('Button2').addEventListener('click', function() {location.href = 'OnCampus.html'}, false);
+  document.getElementById("Button2").onclick = function() {myF()};
+  function myF() {
+    window.open('OnCampus.html', '_blank');
+  }
 
 // LinkedIn Button
+  document.getElementById("Button3").addEventListener("mouseover", onLinkedIn);
+
+  function onLinkedIn() {
+            document.getElementById('Button3').innerHTML
+                = 'Go to My LinkedIn';
+        }
+
+  document.getElementById("Button3").addEventListener("mouseout", offLinkedIn);
+
+    function offLinkedIn() {
+              document.getElementById('Button3').innerHTML
+                  = 'LinkedIn';
+          }
+
   document.getElementById("Button3").addEventListener("click", toLinkedIn);
 
     function toLinkedIn() {
