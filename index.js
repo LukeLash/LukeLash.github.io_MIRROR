3a6f4d24d57ba2b924048c6734ff17737c8ad19c
LukeLash <63415678+LukeLash@users.noreply.github.com>
2021-01-11 23:55:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -8,21 +8,63 @@ window.onload = () => {
       element.style.filter  = 'alpha(opacity=90)'; // IE fallback
     }
 
-    // On Campus Button
-      document.getElementById('Button1').addEventListener('click', function() {location.href = 'index.html'}, false);
+// On Campus Button
+  document.getElementById("Button1").addEventListener('click', function() {location.href = 'index.html'}, false);
 
-    // In Industry Button
-      document.getElementById('Button2').addEventListener('click', function() {location.href = 'OnCampus.html'}, false);
+// In Industry Button
+  //document.getElementById('Button2').addEventListener('click', function() {location.href = 'OnCampus.html'}, false);
+  document.getElementById("Button2").onclick = function() {myF()};
+  function myF() {
+    window.open('OnCampus.html', '_blank');
+  }
 
-    // LinkedIn Button
-        document.getElementById("Button3").addEventListener("click", toLinkedIn);
+// LinkedIn Button
+  document.getElementById("Button3").addEventListener("click", toLinkedIn);
 
-        function toLinkedIn() {
-          window.open('https://www.linkedin.com/in/lucas-lasher-21011914a/', '_blank');
-        }
+    function toLinkedIn() {
+      window.open('https://www.linkedin.com/in/lucas-lasher-21011914a/', '_blank');
+    }
 
-    // Contact Button
-      document.getElementById('Button4').addEventListener('click', function() {location.href = 'ConnectWithMe.html'}, false);
+// Contact Button
+  document.getElementById("Button4").addEventListener('click', function() {location.href = 'ConnectWithMe.html'}, false);
+
+
+// Funny quotes playback (for those of you who scrolled down this far)
+document.getElementById("icon1").addEventListener("click", playPulp);
+var audio = false; // declare audio globally so that audio doesn't play in an overlap
+var playPulpDuration = 3.3437 * 1000;
+
+  function playPulp() {
+    if (!audio) {
+      audio = new Audio('marsellus.mp3');
+    }
+    setTimeout(() => {  audio = false; }, playPulpDuration);
+    audio.loop = false;
+    audio.play();
+  }
+
+document.getElementById("icon2").addEventListener("click", playBoats);
+var playBoatsDuration = 2.976 * 1000;
+
+  function playBoats() {
+    if (!audio) {
+      audio = new Audio('boats.mp3');
+    }
+    audio.loop = false;
+    setTimeout(() => {  audio = false; }, playBoatsDuration);
+    audio.play();
+  }
+
+document.getElementById("icon3").addEventListener("click", playTim);
+var playTimDuration = 71.352 * 1000;
+function playTim() {
+  if (!audio) {
+    audio = new Audio('friday_night.mp3');
+  }
+  audio.loop = false;
+  setTimeout(() => {  audio = false; }, playTimDuration);
+  audio.play();
+}
 
 
 }
